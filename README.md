80b36f6632aba0339a79c76f104d57601d51aff8
alfieqashwa <alfieqashwa@gmail.com>
2022-04-19 20:35:01 +0700

    fix: rename folder rust-file && update Readme

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # cello-learn-rust again...
+
+## USE CARGO:
+- cargo b
+- cargo t
+- cargo r
+- cargo rr
+
+## USE RUSTC:
+- rustc folder/file.rs
--- rust-file/variables.rs
+++ rust-file/variables.rs
@@ -1,6 +1,6 @@
 fn main() {
   let minus_five = -5;
-  let six: u32 = 6
+  let six: u32 = 6;
 
   assert_eq!(minus_five, -5);
   assert_eq!(six, 6);
