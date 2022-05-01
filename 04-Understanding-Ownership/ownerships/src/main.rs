90cb2d890cb97932c20ce134dfdf24bab67e00e5
alfieqashwa <alfieqashwa@gmail.com>
2022-05-01 23:40:31 +0700

    starts ownership in functions

--- 04-Understanding-Ownership/ownerships/src/main.rs
+++ 04-Understanding-Ownership/ownerships/src/main.rs
@@ -1,6 +1,6 @@
 /*
 Ownership Rules:
-    - Each value in Rust has a variable that’s called its owner.
+   - Each value in Rust has a variable that’s called its owner.
    - There can only be one owner at a time.
    - When the owner goes out of scope, the value will be dropped.
 */
